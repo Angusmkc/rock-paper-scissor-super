7bb1e8022f1fe56e9c17f08dabc4190c509e3b97
Angusmkc <99629386+Angusmkc@users.noreply.github.com>
2022-03-19 20:02:21 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onGesture(Gesture.Shake, function () {
+    retry = true
+    while (retry) {
+        hand = randint(1, 4)
+        if (hand == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # # . .
+                . # # . .
+                . . . . .
+                . . . . .
+                `)
+            retry = false
+        } else if (hand == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            retry = false
+        } else if (hand == 3) {
+            if (_super == 1) {
+                basic.showIcon(IconNames.Surprised)
+                _super += 1
+                retry = false
+            } else {
+                retry = true
+            }
+        } else {
+            basic.showIcon(IconNames.Scissors)
+            retry = false
+        }
+    }
+})
+let hand = 0
+let retry = false
+let _super = 0
+_super = 1
