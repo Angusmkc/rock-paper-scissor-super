8b604fa13d18b4fb3ae30c4363346652afa1f124
Angusmkc <99629386+Angusmkc@users.noreply.github.com>
2023-09-09 22:46:55 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,84 @@
-input.onGesture(Gesture.Shake, function () {
-    retry = true
-    while (retry) {
-        hand = randint(1, 4)
-        if (hand == 1) {
-            basic.showLeds(`
-                . . . . .
-                . # # . .
-                . # # . .
-                . . . . .
-                . . . . .
-                `)
-            retry = false
-        } else if (hand == 2) {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            retry = false
-        } else if (hand == 3) {
-            if (_super == 1) {
-                basic.showLeds(`
-                    # # . # #
-                    # . # . #
-                    . # . # .
-                    # . # . #
-                    # # . # #
-                    `)
-                _super += 1
-                retry = false
-            } else {
-                retry = true
-            }
-        } else {
-            basic.showIcon(IconNames.Scissors)
-            retry = false
-        }
+function restart () {
+    music.play(music.createSoundExpression(WaveShape.Noise, 5000, 3488, 255, 67, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    user_score = 0
+    comp_score = 0
+    return 0
+}
+function rockpaperSissors () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
     }
+    return 0
+}
+input.onButtonPressed(Button.A, function () {
+    user_score += 1
+    music.play(music.createSoundExpression(WaveShape.Square, 3426, 1, 216, 0, 359, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+    scores(user_score, comp_score)
 })
+input.onSound(DetectedSound.Loud, function () {
+    rockpaperSissors()
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    comp_score += 1
+    music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+    scores(user_score, comp_score)
+})
+function scores (user: number, comp: number) {
+    score = "" + user + ":" + comp
+    basic.showString(score)
+    radio.sendString(score)
+    if (user == 2) {
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Happy)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        restart()
+    }
+    if (comp == 2) {
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Sad)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        restart()
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    return 0
+}
+let score = ""
 let hand = 0
-let retry = false
-let _super = 0
-_super = 1
+let comp_score = 0
+let user_score = 0
+restart()
